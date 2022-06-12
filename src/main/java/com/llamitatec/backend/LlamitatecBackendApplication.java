4ad651cd9196ee77906c1229859cae3c9db5a63d
FrancoVasquez <u201920339@upc.edu.pe>
2022-06-12 17:23:45 -0500

    Initial commit

---
+++ src/main/java/com/llamitatec/backend/LlamitatecBackendApplication.java
@@ -0,0 +1,13 @@
+package com.llamitatec.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlamitatecBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LlamitatecBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/llamitatec/backend/LlamitatecBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.llamitatec.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlamitatecBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
