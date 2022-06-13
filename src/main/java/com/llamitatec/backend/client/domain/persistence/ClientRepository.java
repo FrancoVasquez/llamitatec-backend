81a1b9d641b9464c137d050f70d2af40ea5b868c
Williamz21 <u201816224@upc.edu.pe>
2022-06-13 02:46:39 -0500

    feat: client resource implemented

--- src/main/java/com/llamitatec/backend/client/domain/persistence/ClientRepository.java
+++ src/main/java/com/llamitatec/backend/client/domain/persistence/ClientRepository.java
@@ -4,9 +4,11 @@ import com.llamitatec.backend.client.domain.model.entity.Client;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
     List<Client> findByUserId(Long userId);
     Page<Client> findByUserId(Long userId, Pageable pageable);
