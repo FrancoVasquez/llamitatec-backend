9ef32946c4754e4f2f28b13eced44521e854692b
Williamz21 <u201816224@upc.edu.pe>
2022-06-13 04:01:41 -0500

    feat: service resource implemented

--- src/main/java/com/llamitatec/backend/service/domain/persistence/ServiceRepository.java
+++ src/main/java/com/llamitatec/backend/service/domain/persistence/ServiceRepository.java
@@ -2,7 +2,9 @@ package com.llamitatec.backend.service.domain.persistence;
 
 import com.llamitatec.backend.service.domain.model.entity.Service;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ServiceRepository extends JpaRepository<Service,Long> {
     Service findByName(String name);
 }
---
+++ src/main/java/com/llamitatec/backend/service/resource/ServiceResource.java
@@ -0,0 +1,12 @@
+package com.llamitatec.backend.service.resource;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ServiceResource {
+    private String name;
+    private String urlToImage;
+    private String description;
+}
