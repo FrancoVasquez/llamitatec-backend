2fc2d7fa59d4fc27d7ad469f102ffef8e49f5a03
Williamz21 <u201816224@upc.edu.pe>
2022-06-26 20:09:05 -0500

    feat: security created

--- src/main/java/com/llamitatec/backend/security/mapping/MappingConfiguration.java
+++ src/main/java/com/llamitatec/backend/security/mapping/MappingConfiguration.java
@@ -1,11 +1,15 @@
-package com.llamitatec.backend.user.mapping;
+package com.llamitatec.backend.security.mapping;
 
+import com.llamitatec.backend.user.mapping.UserMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration("userMappingConfiguration")
+@Configuration("securityMappingConfiguration")
 public class MappingConfiguration {
-
+    @Bean
+    public RoleMapper roleMapper() {
+        return new RoleMapper();
+    }
     @Bean
     public UserMapper userMapper(){
         return new UserMapper();
